b4fded86d3d094202e72261a7eadad942fbf36c0
Tanaka Akira <akr@fsij.org>
2013-02-26 18:59:07 +0900

    scripts/cygwin-tests.sh should also be used for Haiku.

--- run-test.sh
+++ run-test.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
-case "`uname -srvm`" in
-Linux*)
-  sh scripts/basic-tests.sh
+UNAME="`uname -srvm`"
+
+sh scripts/basic-tests.sh
+
+case "$UNAME" in
+CYGWIN*|Haiku*)
+  sh scripts/nullless-simple-tests.sh
+  ;;
+*)
   sh scripts/nullless-tests.sh
+  ;;
+esac
+
+case "$UNAME" in
+Linux*)
   sh scripts/linux-tests.sh
   ;;
 NetBSD*|FreeBSD*|OpenBSD*|DragonFly*|MirBSD*|GNU/kFreeBSD*|*Hurd*|Darwin*|Haiku*)
-  sh scripts/basic-tests.sh
-  sh scripts/nullless-tests.sh
   sh scripts/44bsd-tests.sh
-  ;;
-SunOS*|Minix*)
-  sh scripts/basic-tests.sh
-  sh scripts/nullless-tests.sh
-  ;;
-CYGWIN*)
-  sh scripts/basic-tests.sh
-  sh scripts/cygwin-tests.sh
-  ;;
-*)
-  sh scripts/basic-tests.sh
   ;;
 esac
