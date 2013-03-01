51e3aad99bec82d1b34e9bf557c83683dc5b4486
Tanaka Akira <akr@fsij.org>
2013-03-02 03:11:16 +0900

    refine header inclusion mechanism.

--- sockettest.h
+++ sockettest.h
@@ -44,75 +44,7 @@
 #  define _XOPEN_SOURCE 600
 #endif
 
-/* AC_INCLUDES_DEFAULT */
-#include <stdio.h>
-#ifdef HAVE_SYS_TYPES_H
-#  include <sys/types.h>
-#endif
-#ifdef HAVE_SYS_STAT_H
-#  include <sys/stat.h>
-#endif
-#ifdef STDC_HEADERS
-#  include <stdlib.h>
-#  include <stddef.h>
-#else
-#  ifdef HAVE_STDLIB_H
-#    include <stdlib.h>
-#  endif
-#endif
-#ifdef HAVE_STRING_H
-#  if !defined STDC_HEADERS && defined HAVE_MEMORY_H
-#    include <memory.h>
-#  endif
-#  include <string.h>
-#endif
-#ifdef HAVE_STRINGS_H
-#  include <strings.h>
-#endif
-#ifdef HAVE_INTTYPES_H
-#  include <inttypes.h>
-#endif
-#ifdef HAVE_STDINT_H
-#  include <stdint.h>
-#endif
-#ifdef HAVE_UNISTD_H
-#  include <unistd.h>
-#endif
-
-#ifdef HAVE_LIMITS_H
-#  include <limits.h>
-#endif
-
-#ifdef HAVE_ERRNO_H
-#  include <errno.h>
-#endif
-#ifdef HAVE_CTYPE_H
-#  include <ctype.h>
-#endif
-#ifdef HAVE_FLOAT_H
-#  include <float.h>
-#endif
-#ifdef HAVE_TIME_H
-#  include <time.h>
-#endif
-#ifdef HAVE_SIGNAL_H
-#  include <signal.h>
-#endif
-#ifdef HAVE_SYS_WAIT_H
-#  include <sys/wait.h>
-#endif
-#ifdef HAVE_SYS_UIO_H
-#  include <sys/uio.h>
-#endif
-#ifdef HAVE_SYS_SOCKET_H
-#  include <sys/socket.h>
-#endif
-#ifdef HAVE_SYS_UN_H
-#  include <sys/un.h>
-#endif
-#ifdef HAVE_NETINET_IN_H
-#  include <netinet/in.h>
-#endif
+#include "includes.h"
 
 #define FIELD_SIZE(struct_name, field_name) sizeof(((struct_name *)0)->field_name)
 #define ALIGNOF(type) ((int)offsetof(struct { char f1; type f2; }, f2))
