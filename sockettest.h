ff5f75e1fb2f831dea4be55cc48e110c3696cd73
Tanaka Akira <akr@fsij.org>
2013-03-02 01:15:00 +0900

    add guard for header inclusion.

--- sockettest.h
+++ sockettest.h
@@ -44,22 +44,53 @@
 #  define _XOPEN_SOURCE 600
 #endif
 
-#include <stddef.h>
-#include <stdlib.h>
+/* AC_INCLUDES_DEFAULT */
 #include <stdio.h>
-#include <inttypes.h>
+#ifdef HAVE_SYS_TYPES_H
+#  include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_STAT_H
+#  include <sys/stat.h>
+#endif
+#ifdef STDC_HEADERS
+#  include <stdlib.h>
+#  include <stddef.h>
+#else
+#  ifdef HAVE_STDLIB_H
+#    include <stdlib.h>
+#  endif
+#endif
+#ifdef HAVE_STRING_H
+#  if !defined STDC_HEADERS && defined HAVE_MEMORY_H
+#    include <memory.h>
+#  endif
+#  include <string.h>
+#endif
+#ifdef HAVE_STRINGS_H
+#  include <strings.h>
+#endif
+#ifdef HAVE_INTTYPES_H
+#  include <inttypes.h>
+#endif
+#ifdef HAVE_STDINT_H
+#  include <stdint.h>
+#endif
+#ifdef HAVE_UNISTD_H
+#  include <unistd.h>
+#endif
+
+#ifdef HAVE_LIMITS_H
+#  include <limits.h>
+#endif
+#ifdef HAVE_SYS_SOCKET_H
+#  include <sys/socket.h>
+#endif
+
 #include <ctype.h>
-#include <string.h>
-#include <unistd.h>
-#include <limits.h>
-#include <sys/types.h>
 #include <sys/uio.h>
-#include <sys/socket.h>
 #include <sys/un.h>
 #include <netinet/in.h>
-#include <sys/stat.h>
 #include <errno.h>
-#include <limits.h>
 #include <float.h>
 #include <time.h>
 #include <sys/wait.h>
