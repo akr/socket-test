8cfc38cede4b233e996d4dc2b328131026fb9145
Tanaka Akira <akr@fsij.org>
2013-03-03 19:44:15 +0900

    don't test unix unix-dgram-basic-tests.sh on Hurd and Haiku.

--- run-test.sh
+++ run-test.sh
@@ -15,7 +15,10 @@ Linux*) sh scripts/unix-stream-linux-tests.sh ;;
 NetBSD*|FreeBSD*|OpenBSD*|DragonFly*|MirBSD*|GNU/kFreeBSD*|*Hurd*|Darwin*|Haiku*) sh scripts/unix-stream-44bsd-tests.sh ;;
 esac
 
-sh scripts/unix-dgram-basic-tests.sh
+case "$UNAME" in
+*Hurd*|Haiku*) : ;;
+*) sh scripts/unix-dgram-basic-tests.sh;;
+esac
 
 case "$UNAME" in
 CYGWIN*|Haiku*|Minix*) sh scripts/unix-dgram-nullless-simple-tests.sh ;;
